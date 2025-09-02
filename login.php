059ca03e5fe5667f41ac49c1dd33204fa9c59ddb
Victor <thehunter.modd@gmail.com>
2025-09-02 12:35:08 +0100

    sixth init

--- login.php
+++ login.php
@@ -6,60 +6,38 @@ require_once 'includes/signin_view.inc.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" integrity="sha512-fw7f+TcMjTb7bpbLJZlP8g2Y4XcCyFZW8uy8HsRZsH/SwbMw0plKHFHr99DN3l04VsYNwvzicUX/6qurvIxbxw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="CSS/style.css">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Staff Login - LASUED Bus Ticketing</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" crossorigin="anonymous" />
+  <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
-    <h3>
-    <?php output_staff_id(); 
-    ?>
-  </h3>
 
-    <h3 class="ls-heading">Login</h3>
+  <div class="d-flex justify-content-center align-items-center vh-100">
+    <div class="col-md-6 col-lg-4">
+      <div class="card shadow-lg rounded-3 p-4">
+        <h3 class="ls-heading mb-3">Staff Login Portal</h3>
+        <p class="welcome-message">Access the LASUED Staff Bus Ticketing System</p>
 
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-6 col-lg-4"> <!-- Adjust width here -->
-      
-                    <form class="row g-3" action="includes/signin.inc.php" method="post">
-                        <div class="col-12">
-                            <input type="text" name="staff_id" class="form-control" placeholder="Staff Id">
-                        </div>
-                        <div class="col-12">
-                            <input type="password" name="password" class="form-control" placeholder="Password">
-                        </div>
-                        <div class="col-12 text-center">
-                            <button type="submit" class="btn btn-primary">Login</button>
-                        </div>
-                    </form>
+        <form class="row g-3" action="includes/signin.inc.php" method="post">
+          <div class="col-12">
+            <input type="text" name="staff_id" class="form-control" placeholder="Staff ID" required>
+          </div>
+          <div class="col-12">
+            <input type="password" name="password" class="form-control" placeholder="Password" required>
+          </div>
+          <div class="col-12 text-center">
+            <button type="submit" class="btn btn-primary w-100">Login</button>
+          </div>
+        </form>
 
-                </div>     
-            </div>
-        </div> 
-
-        <div class="row justify-content-center">
-            <div class="col-md-6 col-lg-4">
-                <?php 
-                Check_signin_errors(); 
-                ?>
-            </div>
+        <div class="mt-3 text-center">
+          <?php Check_signin_errors(); ?>
         </div>
-
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-6 col-lg-4"> <!-- Adjust width here -->
-                    <form class="row g-3" action="includes/signout.inc.php" method="post">
-                        <div class="col-12 text-center">
-                            <button type="submit" class="btn btn-primary">Logout</button>
-                        </div>
-                    </form>
-      
-                </div>
-            </div>
-        </div>
+      </div>
+    </div>
+  </div>
 
 </body>
 </html>
--- register.php
+++ register.php
@@ -8,46 +8,41 @@ require_once 'includes/signup_view.inc.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" integrity="sha512-fw7f+TcMjTb7bpbLJZlP8g2Y4XcCyFZW8uy8HsRZsH/SwbMw0plKHFHr99DN3l04VsYNwvzicUX/6qurvIxbxw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="CSS/style.css">
-    <title>Document</title>
+    <title>Register | LASUED Staff Bus</title>
 </head>
-<body>
+<body class="bg-light">
 
-    <h3 class="ls-heading">Register</h3>
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="card shadow-lg p-4 rounded-4" style="max-width: 400px; width: 100%;">
+            
+            <h3 class="ls-heading mb-3 text-primary">Create Account</h3>
+            <p class="welcome-message">Register to book your staff bus tickets</p>
 
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-6 col-lg-4"> <!-- Adjust width here -->
-      
-                    <form class="row g-3" action="includes/signup.inc.php" method="post">
-                        <div class="col-12">
-                            <input type="text" name="staff_id" class="form-control" placeholder="Staff Id">
-                        </div>
-                        <div class="col-12">
-                            <input type="text" name="name" class="form-control" placeholder="Full Name">
-                        </div>
-                        <div class="col-12">
-                            <input type="email" name="email" class="form-control" placeholder="Email">
-                        </div>
-                        <div class="col-12">
-                            <input type="password" name="password" class="form-control" placeholder="Password">
-                        </div>
-                        <div class="col-12 text-center">
-                            <button type="submit" class="btn btn-primary">Register</button>
-                        </div>
-                    </form>
+            <form action="includes/signup.inc.php" method="post" class="row g-3">
+                <div class="col-12">
+                    <input type="text" name="staff_id" class="form-control" placeholder="Staff ID" >
+                </div>
+                <div class="col-12">
+                    <input type="text" name="name" class="form-control" placeholder="Full Name" >
+                </div>
+                <div class="col-12">
+                    <input type="email" name="email" class="form-control" placeholder="Email" >
+                </div>
+                <div class="col-12">
+                    <input type="password" name="password" class="form-control" placeholder="Password" >
+                </div>
+                <div class="col-12 text-center">
+                    <button type="submit" class="btn btn-primary w-100 rounded-3">Register</button>
+                </div>
+            </form>
 
-                </div>     
-            </div>
-        </div> 
-        <div class="row justify-content-center">
-            <div class="col-md-6 col-lg-4">
-                <?php 
-                Check_signup_errors(); 
-                ?>
+            <div class="mt-3 text-center">
+                <?php Check_signup_errors(); ?>
             </div>
         </div>
+    </div>
 
 </body>
 </html>
